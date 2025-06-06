b075351ae8ec009f8b64e7c298710008da72e420
Matheus de Alencar <sletezes@gmail.com>
2025-06-06 08:49:52 -0300

    tentativa 1

--- pwm.v
+++ pwm.v
@@ -6,6 +6,16 @@ module PWM (
     output reg pwm_out
 );
 
-//insira o seu código aqui
-    
+    //insira o seu código aqui
+    reg [31:0] count;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            count   <= 32'b0;
+            pwm_out <= 1'b0;
+        end else begin
+            pwm_out <= (count < duty_cycle)  ? 1'b1    : 1'b0;
+            count   <= (count < period-1)    ? count+1 : 32'b0;
+        end
+    end
 endmodule
